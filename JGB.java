3a4741cf92d1f9a17220dfbaeb671aeb04fa45af
Ifte <156860282+Ifte-Ace@users.noreply.github.com>
2024-10-15 15:08:10 -0400

    Create JGB.java

---
+++ JGB.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+public class Lab1 {
+ 
+public static void main(String[] args) {
+ 
+ 
+Scanner console = new Scanner(System.in);
+ 
+System.out.print("What percentage did you earn? ");
+ 
+int percent = console.nextInt();
+ 
+if (percent >= 90) {
+ 
+System.out.println("You got an A!");
+ 
+}
+ 
+else if (percent >= 80) {
+ 
+System.out.println("You got a B!");
+ 
+}
+ 
+else if (percent >= 70) {
+ 
+System.out.println("You got a C!");
+ 
+}
+ 
+else if (percent >= 60) {
+ 
+System.out.println("You got a D!");
+ 
+}
+ 
+else {
+ 
+System.out.println("You got an F!");
+ 
+}
+ 
+}
